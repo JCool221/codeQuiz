48699c22be50a6a038cd8db4bea753275fbca7fd
JCool221 <109809337+JCool221@users.noreply.github.com>
2022-10-07 09:41:24 -0400

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,28 @@
-var displayEl = $('#display');
-var startButton = $('#start-btn')
-
+var displayEl = document.querySelector('#display');
+var startButton = document.querySelector('#start-btn');
+var secondsLeft = 10;
+var timerEl = document.querySelector('#timer');
 // add timer
+function countdown () {
+    var timerInterval= setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = "timer: " + secondsLeft;
+
+        if(secondsLeft === 0 ) {
+            clearInterval(timerInterval);
+            testing123();
+        }
+    }, 1000);
+}
 // store and recover high scores
 // create questions
 
 
 // write something to the text display box
-displayEl.text('Try to answer the following code related questions withihn the time limit. keep in mind that incorrect answers will penalize your scoretime by ten seconds!');
+displayEl.textContent = 'Try to answer the following code related questions withihn the time limit. keep in mind that incorrect answers will penalize your scoretime by ten seconds!';
 
 // event handler for start button
-startButton.on('click', screenTest);
+startButton.addEventListener('click', screenTest);
 
 
 // test functions delete before deployment
@@ -18,5 +30,6 @@ function testing123() {
     console.log('test');
 }
 function screenTest() {
-    displayEl.text('testing');
+    countdown();
+    displayEl.textContent = 'testing';
 }
